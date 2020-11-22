8ebf12e695449a9d76ed4f5bc36fb1fe08415c63
Waleed3mar <72344503+Waleed3mar@users.noreply.github.com>
2020-11-22 08:27:05 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# Landing-Page-Project-Udacity
+This is the my Udacity First Project!
+
+## Notes
+
+* This project is about making a dynmic nav bar and styling while scrolling
+
+* This project used http://15.236.9.38/landing-project-udacity as template for HTML & CSS ONLY!
+
+* A lot of various changes to html and Css have been made!
+
+## Content
+- **app.js**, which is the javascript file that has _three_ sections:
+  1. Creating links in unorderd list   
+  1. Adding unorderd list to navbar which is already created in HTML as tag   
+  1. Check for sections positions to pass two classs if they are at the view Point, the two classs are for [ nav bar and links ]
+    
+- **index.html**, which contains all html tags [sections, nav and footer]
+
+- **style.css**, which contains all css selectors and selectors for the passed classs
---
+++ js/app.js
@@ -0,0 +1,47 @@
+
+//links creation
+const sections = document.querySelectorAll('section');
+const fragment = document.createDocumentFragment();
+const ullist = document.createElement('ul')
+fragment.appendChild(ullist);
+
+for(let i = 0; i < sections.length; i++){
+
+    const lione = document.createElement('li');
+    ullist.appendChild(lione);
+    lione.innerHTML = `<a class="link" href="#${sections[i].id}">${sections[i].id}</a>`; 
+
+}
+//adding links to Html
+const setInNav = document.querySelector('nav');
+setInNav.appendChild(fragment);
+
+//Styling
+const navLinks = document.getElementsByClassName('link');
+const sec = document.getElementsByClassName('section');
+function isInViewport(i) 
+{
+    const rect = sections[i].getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.left >= 0 &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+    
+};
+
+for (let i = 0; i < sec.length; i++) {
+    document.addEventListener("scroll", function() 
+    {
+        if (isInViewport(i)===true){
+            sec[i].classList.add('your-active-class')
+            navLinks[i].classList.add('active-link')
+        }
+        else {
+            sec[i].classList.remove('your-active-class')
+            navLinks[i].classList.remove('active-link')
+        }
+    }
+  );
+}
